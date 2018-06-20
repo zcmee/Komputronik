03152cbcd30aa02600c47a04d48231ac31e58f14
Adam <zcm@interia.pl>
2018-06-20 20:54:31 +0200

    Include Model mapper

--- Comp/src/main/java/com/github/zcmee/komputronik/CompApplication.java
+++ Comp/src/main/java/com/github/zcmee/komputronik/CompApplication.java
@@ -1,12 +1,19 @@
 package com.github.zcmee.komputronik;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableConfigurationProperties
 public class CompApplication {
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CompApplication.class, args);
