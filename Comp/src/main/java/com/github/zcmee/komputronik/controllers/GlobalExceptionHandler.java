1b31c6c01b19c70e6cb51a8096dfe5417441a6f8
adam.bryksy <adam.bryksy@ramsat.pl>
2018-06-29 12:57:20 +0200

    wrap objects to try-with-resources method (reverted from commit a32c5b730f5cc6b276031639bd2922cbfe2bc044)

--- Comp/src/main/java/com/github/zcmee/komputronik/controllers/GlobalExceptionHandler.java
+++ Comp/src/main/java/com/github/zcmee/komputronik/controllers/GlobalExceptionHandler.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.List;
@@ -41,17 +40,12 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public void handle(Exception exception) throws IOException {
-        
-        try(
-            StringWriter sw = new StringWriter();
-            PrintWriter pw = new PrintWriter(sw)
-        ){
-            exception.printStackTrace(pw);
-            String sStackTrace = sw.toString();
-            logger.error(sStackTrace,exception);
-        }
-
+    public void handle(Exception exception) {
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        exception.printStackTrace(pw);
+        String sStackTrace = sw.toString();
+        logger.error(sStackTrace,exception);
     }
 
 }
